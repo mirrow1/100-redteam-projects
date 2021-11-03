503452c4f2e3fa42340d51cd42224ce4224b7eb6
mirrow1 <44827973+mirrow1@users.noreply.github.com>
2021-11-04 00:00:15 +1300

    Update 2.py

--- Projects/project2/2.py
+++ Projects/project2/2.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python3
 
 import socket
-from threading import *
 
 host,port = "127.0.0.1",9999
-
 server = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-
 server.bind((host,port))
-
-#server.listen(5)
-
-#client,addr = server.accept()
 
 clients = []
 
@@ -23,9 +16,6 @@ while True:
 		clients.append(addr)
 
 	for c in clients:
-
 		if c != addr:
-
 			sender = bytes("(" + addr[0] + "," + str(addr[1]) + ")# ","utf-8")
-
 			server.sendto((sender+message),c)
