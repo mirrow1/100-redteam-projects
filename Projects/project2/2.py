d8cb5f76122afa00cad115b17ef61b42694362ce
mirrow1 <44827973+mirrow1@users.noreply.github.com>
2021-11-03 22:51:04 +1300

    Create 2.py

---
+++ Projects/project2/2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import socket
+from threading import *
+
+host,port = "127.0.0.1",9999
+
+server = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+server.bind((host,port))
+
+#server.listen(5)
+
+#client,addr = server.accept()
+
+clients = []
+
+while True:
+
+	message,addr = server.recvfrom(4096)
+
+	if addr not in clients:
+		clients.append(addr)
+
+	for c in clients:
+
+		if c != addr:
+
+			sender = bytes("(" + addr[0] + "," + str(addr[1]) + ")# ","utf-8")
+
+			server.sendto((sender+message),c)
